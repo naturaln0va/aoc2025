76ed0a5697636c792aabf397dfbdacae44bc1f86
Ryan Ackermann <ryan.ackermann@a8c.com>
2025-12-02 11:51:06 -0800

    rubyfmt

--- day1.rb
+++ day1.rb
@@ -1,60 +1,60 @@
 # This script was written for the advent of code 2025 day 1.
 # https://adventofcode.com/2025/day/1
 
-require 'uri'
-require 'net/http'
+require "uri"
+require "net/http"
 
 class Solver
   def initialize(day, year = 2025)
-		@day = day
-		@year = year
+    @day = day
+    @year = year
   end
 
-	def fetch_input
-		input_directory = 'input'
-		unless Dir.exist? input_directory
-			Dir.mkdir input_directory
-		end
-		
-		input_file_name = "#{@year}-#{@day}-input.txt"
-		input_file_path = File.join input_directory, input_file_name
-		
-		if File.exist? input_file_path
-			input_file = File.open input_file_path
-			@input = input_file.read.chomp
-			input_file.close
-			return
-		end
-		
-		auth_filename = 'cookie.txt'
-		unless File.exist? auth_filename
-			abort('"cookie.txt" is required to get the puzzle input for your account.')
-		end
-	
-		cookie_file = File.open auth_filename
-		
-		uri = URI("https://adventofcode.com/#{@year}/day/#{@day}/input")
-		cookie_value = cookie_file.read.chomp
-		user_agent = 'github.com/naturaln0va/aoc2025 by Ryan Ackermann'
-		headers = { 'Cookie' => "session=#{cookie_value}", 'User-Agent' => user_agent }
-		
-		cookie_file.close
-		
-		puts "fetching the puzzle input for day #{@day}..."
-		res = Net::HTTP.get_response(uri, headers)
-	
-		unless res.is_a? Net::HTTPSuccess
-			abort("HTTP Error: #{res.code} - #{res.body}")
-		end
-		
-		@input = res.body
-		
-		File.write(input_file_path, @input)
-	end
+  def fetch_input
+    input_directory = "input"
+    unless Dir.exist?(input_directory)
+      Dir.mkdir(input_directory)
+    end
+
+    input_file_name = "#{@year}-#{@day}-input.txt"
+    input_file_path = File.join(input_directory, input_file_name)
+
+    if File.exist?(input_file_path)
+      input_file = File.open(input_file_path)
+      @input = input_file.read.chomp
+      input_file.close
+      return
+    end
+
+    auth_filename = "cookie.txt"
+    unless File.exist?(auth_filename)
+      abort("\"cookie.txt\" is required to get the puzzle input for your account.")
+    end
+
+    cookie_file = File.open(auth_filename)
+
+    uri = URI("https://adventofcode.com/#{@year}/day/#{@day}/input")
+    cookie_value = cookie_file.read.chomp
+    user_agent = "github.com/naturaln0va/aoc2025 by Ryan Ackermann"
+    headers = {"Cookie" => "session=#{cookie_value}", "User-Agent" => user_agent}
+
+    cookie_file.close
+
+    puts("fetching the puzzle input for day #{@day}...")
+    res = Net::HTTP.get_response(uri, headers)
+
+    unless res.is_a?(Net::HTTPSuccess)
+      abort("HTTP Error: #{res.code} - #{res.body}")
+    end
+
+    @input = res.body
+
+    File.write(input_file_path, @input)
+  end
 
   def test_case
-		puts "===TEST==="
-		first_test_input = "L68
+    puts("===TEST===")
+    first_test_input = "L68
 		L30
 		R48
 		L5
@@ -64,95 +64,95 @@ class Solver
 		L99
 		R14
 		L82"
-		first_answer = solve_first(first_test_input)
-		puts "1st answer: #{first_answer}"
-		second_test_input = first_test_input
-		second_answer = solve_second(second_test_input)
-		puts "2nd answer: #{second_answer}"
+    first_answer = solve_first(first_test_input)
+    puts("1st answer: #{first_answer}")
+    second_test_input = first_test_input
+    second_answer = solve_second(second_test_input)
+    puts("2nd answer: #{second_answer}")
   end
 
   def decipher
-		fetch_input
-		puts "===PUZZLE==="
-		first_answer = solve_first(@input)
-		puts "1st answer: #{first_answer}"
-		second_answer = solve_second(@input)
-		puts "2nd answer: #{second_answer}"
+    fetch_input
+    puts("===PUZZLE===")
+    first_answer = solve_first(@input)
+    puts("1st answer: #{first_answer}")
+    second_answer = solve_second(@input)
+    puts("2nd answer: #{second_answer}")
   end
 
   def solve_first(input)
-		answer = 0
-		lines = input.lines.map(&:strip)
-		cmds = input.split()
-		
-		dial = 50
-		
-		cmds.each do |cmd|
-			dir = cmd.slice!(0)
-			to_move = cmd.to_i % 100
-			if dir == "L"
-				# puts "turning left #{cmd}"
-				dial -= to_move
-			else
-				# puts "turning right #{cmd}"
-				dial += to_move
-			end
-			
-			if dial < 0
-				adjusted = 100 + dial
-				# puts "adjusting #{dial} to #{adjusted}"
-				dial = adjusted
-			elsif dial > 99
-				adjusted = dial - 100
-				# puts "adjusting #{dial} to #{adjusted}"
-				dial = adjusted
-			end
-			
-			if dial == 0
-				answer += 1
-			end
-		end
-		
-		answer
+    answer = 0
+    lines = input.lines.map(&:strip)
+    cmds = input.split
+
+    dial = 50
+
+    cmds.each do |cmd|
+      dir = cmd.slice!(0)
+      to_move = cmd.to_i % 100
+      if dir == "L"
+        # puts "turning left #{cmd}"
+        dial -= to_move
+      else
+        # puts "turning right #{cmd}"
+        dial += to_move
+      end
+
+      if dial < 0
+        adjusted = 100 + dial
+        # puts "adjusting #{dial} to #{adjusted}"
+        dial = adjusted
+      elsif dial > 99
+        adjusted = dial - 100
+        # puts "adjusting #{dial} to #{adjusted}"
+        dial = adjusted
+      end
+
+      if dial == 0
+        answer += 1
+      end
+    end
+
+    answer
   end
 
   def solve_second(input)
-		answer = 0
-		lines = input.lines.map(&:strip)
-		
-		cmds = input.split()
-		
-		dial = 50
-		
-		cmds.each do |cmd|
-			dir = cmd.slice!(0)
-			to_move = cmd.to_i
-			sign = dir == "L" ? -1 : 1
-			
-			(0...to_move).each do |i|
-				dial += 1 * sign
-				# puts "turned to #{dial}"
-				if dial == 0
-					# puts "landed on zero"
-				elsif dial < 0
-					adjusted = 100 + dial
-					# puts "adjusting #{dial} to #{adjusted}"
-					dial = adjusted
-				elsif dial > 99
-					adjusted = dial - 100
-					# puts "adjusting #{dial} to #{adjusted}"
-					dial = adjusted
-				end
-				
-				answer += 1 if dial == 0
-			end
-		end
-		
-		answer
+    answer = 0
+    lines = input.lines.map(&:strip)
+
+    cmds = input.split
+
+    dial = 50
+
+    cmds.each do |cmd|
+      dir = cmd.slice!(0)
+      to_move = cmd.to_i
+      sign = dir == "L" ? -1 : 1
+
+      (0...to_move).each do |i|
+        dial += 1 * sign
+        # puts "turned to #{dial}"
+        if dial == 0
+          # puts "landed on zero"
+        elsif dial < 0
+          adjusted = 100 + dial
+          # puts "adjusting #{dial} to #{adjusted}"
+          dial = adjusted
+        elsif dial > 99
+          adjusted = dial - 100
+          # puts "adjusting #{dial} to #{adjusted}"
+          dial = adjusted
+        end
+
+        answer += 1 if dial == 0
+      end
+    end
+
+    answer
   end
 end
 
 s = Solver.new(day = 1)
 s.test_case
-puts ""
+puts("")
 s.decipher
